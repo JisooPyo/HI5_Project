6ea4bcb999fc62803aca35b5b103344696126341
happiipark <124240328+happiipark@users.noreply.github.com>
2023-06-05 10:49:45 +0900

    first
    first

---
+++ src/Amend.java
@@ -0,0 +1,11 @@
+public class Amend {
+    public Amend(int number){
+        if(number.isEmpty()){
+            System.out.println("수정할 글이 존재하지 않습니다.");
+        } else{
+            System.out.println("수정 글 존재");
+        }
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
+
+    static Map<Integer, String> map= new HashMap<Integer, String>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main( String[] args ) {
-        System.out.println( "Hello world!" );
+//        System.out.println(map.keySet()); // 키 값 확인
+        System.out.print("수정할 글 번호를 입력하세요 :" );
+        int number = sc.nextInt();
+
+
+         Amend amend = new Amend(number);
+
     }
 }
