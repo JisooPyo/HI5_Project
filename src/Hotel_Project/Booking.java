efd3b96faedf1f3ecab7c7f231c4b039bde81648
JisooPyo <audwl326@gmail.com>
2023-06-07 06:18:52 +0900

    showBookingToGuest() 메서드 구현 완료
    1. showBookingToGuest() 메서드 구현 완료
    2. 추후 cancelBookingCheck() 메서드에서 bookingList.remove 잘 실행되는지 확인할 것.
    3. Hotel 클래스 마지막 queue가 double로 잘못 표기되어있어서 suite로 정정

--- src/Hotel_Project/Booking.java
+++ src/Hotel_Project/Booking.java
@@ -7,6 +7,7 @@ public class Booking {
     Room room;
     Integer idNumber = 1000;
     Hotel hotel = new Hotel();
+    Scanner sc = new Scanner( System.in );
     Map< Integer, Queue< Room > > mapRoom = hotel.makeMapRoom();
     // Integer에는 1,2,3,4가 들어갑니다.
     Map< Integer, Booking > mapBooking = new HashMap<>();
@@ -56,12 +57,10 @@ public class Booking {
     ////////////////////////// 지수 구현부/////////////////////////////////////////////////
 
     public void showBookingToGuest() {
-        // mapGuest를 이용하여 idNumber에 맞는 Value값(Booking)을 찾아옵니다.
-        // idNumber가 맞지 않으면 "ID에 맞는 예약 현황이 없습니다."를 출력합니다.
+        // mapBooking을 이용하여 idNumber에 맞는 Value값(Booking)을 찾아옵니다.
         // idNumber가 일치하면 [예약 현황]을 보여주고 1. 메인메뉴 2. 예약취소를 출력합니다.
-        // 1번을 선택하면 메서드종료(BookProgram 클래스로 돌아가고 메인메뉴로 돌아갑니다.)
         // 2번을 선택하면 예약을 취소할지 물어보고 1. 취소 2. 취소 안함
-        // 1번을 선택하면 예약 취소 -> mapRoom 추가, mapGuest 빼기, bookingList 빼기
+        // 1번을 선택하면 예약 취소 -> mapRoom 추가, mapBooking 빼기, bookingList 빼기
         if ( bookingList.size() == 0 ) {
             System.out.println( "예약현황이 없습니다." );
             System.out.println( "메인화면으로 돌아갑니다.\n" );
@@ -71,24 +70,83 @@ public class Booking {
         System.out.println( "예약조회를 위해 idNumber가 필요합니다." );
         System.out.println( "idNumber을 입력해주세요." );
 
-        Scanner sc = new Scanner( System.in );
-        Integer inputIdNum = Integer.valueOf( sc.nextLine() );
+        Integer inputIdNum = Integer.valueOf( sc.next() );
         if ( !mapBooking.keySet().contains( inputIdNum ) ) {
             System.out.println( "ID에 맞는 예약 현황이 없습니다." );
             System.out.println( "메인 화면으로 돌아갑니다.\n" );
             return;
         }
-        cancelBookingcheck( inputIdNum );
+        bookingCheck( inputIdNum );
 
     }
 
-    public void cancelBookingcheck( Integer inputIdnum ) {
-        System.out.println( "[ 예약현황 ]" );
+    public void bookingCheck( Integer inputIdnum ) {
+        System.out.println( "[ 예약현황 ]\n" );
         System.out.println( "예약번호 : " + inputIdnum );
-        System.out.println( "이름 : " + mapBooking.get( inputIdnum ) );
-        System.out.println( "연락처 : " );
-        System.out.println( "예약날짜 : " );
-        System.out.println( "객실크기 : \n" );
+        System.out.println( "이름 : " + mapBooking.get( inputIdnum ).guest.name );
+        System.out.println( "연락처 : " + mapBooking.get( inputIdnum ).guest.phoneNumber );
+        System.out.println( "예약날짜 : " + mapBooking.get( inputIdnum ).guest.date );
+        System.out.println( "객실크기 : \n" + mapBooking.get( inputIdnum ).room.size );
         System.out.println( "1. 메인메뉴 / 2. 예약 취소" );
+
+        int cancelBooking = sc.nextInt();
+        String enter = sc.nextLine();
+
+        switch ( cancelBooking ) {
+            case 1: {
+                return;
+            }
+            case 2: {
+                cancelBookingCheck( inputIdnum );
+                break;
+            }
+            default: {
+                System.out.println( "잘못된 값을 입력하셨습니다. 다시 입력해 주세요." );
+                bookingCheck( inputIdnum );
+                break;
+            }
+        }
+    }
+
+    public void cancelBookingCheck( Integer inputIdNum ) {
+        System.out.println( "예약을 취소하시겠습니까?" );
+        System.out.println( "1. 예 / 2. 아니오" );
+        int cancelPick = sc.nextInt();
+        String enter = sc.nextLine();
+        switch ( cancelPick ) {
+            case 1: {
+                //mapRoom 추가, mapBooking 빼기, bookingList 빼기
+                String roomSize = mapBooking.get( inputIdNum ).room.size;
+                int roomPrice = mapBooking.get( inputIdNum ).room.price;
+                mapRoom.get( getRoomNumber( roomSize ) ).add( new Room( roomSize, roomPrice ) );
+                // bookingList 잘 지워지는지 체크해봐야 함.
+                bookingList.remove( mapBooking.get( inputIdNum ) );
+                mapBooking.remove( inputIdNum );
+                System.out.println("예약이 성공적으로 취소되었습니다.\n");
+                return;
+            }
+            case 2: {
+                System.out.println( "예약현황으로 돌아갑니다.\n" );
+                bookingCheck( inputIdNum );
+                break;
+            }
+            default: {
+                System.out.println( "잘못된 값을 입력하셨습니다. 다시 입력해 주세요." );
+                cancelBookingCheck( inputIdNum );
+                break;
+            }
+        }
+    }
+
+    public Integer getRoomNumber( String size ) {
+        if ( size.equals( "single" ) ) {
+            return 1;
+        } else if ( size.equals( "double" ) ) {
+            return 2;
+        } else if ( size.equals( "twin" ) ) {
+            return 3;
+        } else {    // size.equals("suite")
+            return 4;
+        }
     }
 }
