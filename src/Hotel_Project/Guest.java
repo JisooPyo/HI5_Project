fff03af2e23c18b3254cabb973ac93584cc1a0d3
Hojin37 <hwlsdl37@gmail.com>
2023-06-06 22:08:08 +0900

    guest02

--- src/Hotel_Project/Guest.java
+++ src/Hotel_Project/Guest.java
@@ -33,8 +33,18 @@ public class Guest {
         System.out.println("이름" + ":" + name);
         System.out.println("전화번호" + ":" + phoneNumber);
         System.out.println("소지금" + ":" + money);
-        Guest guest = new Guest( name, phoneNumber, money );
-        return guest;
+        System.out.println("확인하신 내용이 맞다면 1번, 다시 시도하시려면 2번을 입력해주세요.");
+        int confirm = sc.nextInt();
+        if (confirm==1){
+            System.out.println("예약이 완료되었습니다.");
+            Guest guest = new Guest( name, phoneNumber, money );
+            return guest;
+        } else if (confirm==2) {
+            System.out.println("올바른 정보를 다시 입력해주세요.");
+            return inputGuest();
+        } else {
+            System.out.println("잘못 입력하셨습니다. 메인화면으로 돌아갑니다.");
+        }
+        return null;
     }
-
 }
