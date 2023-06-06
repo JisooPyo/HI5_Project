78ee91c77d77d099296f1ba200ec4c87ffb52444
Hojin37 <hwlsdl37@gmail.com>
2023-06-06 23:33:05 +0900

    guest04

--- src/Hotel_Project/Guest.java
+++ src/Hotel_Project/Guest.java
@@ -25,50 +25,67 @@ public class Guest {
         // name, phoneNumber, money를 스캐너로 입력 받아 그 특성을 가지는 Guest를 return하세요.
         Scanner sc = new Scanner( System.in );
         System.out.println("======== 예약하실 분의 성함을 입력해주세요. ========");
+        System.out.println();
         String name = sc.nextLine();
 
+        System.out.println();
         System.out.println("======== 전화번호를 입력해주세요. ========");
         System.out.println("(연락처는 '-'구분없이 숫자만 입력해주세요.)");
+        System.out.println();
         String phoneNumber = sc.nextLine();
 
+        System.out.println();
         System.out.println("======== 소지하고 계신 금액을 입력해주세요. ========");
         System.out.println("(소지금의 단위는 '원'입니다. '원'을 제외하고 숫자만 입력해주세요.)");
+        System.out.println();
         int money = sc.nextInt();
 
+        System.out.println();
         System.out.println("======== 원하시는 예약 날짜를 입력해주세요. ========");
         System.out.println("(yyyy/mm/dd의 형식으로 입력해주세요.)");
+        System.out.println();
         String date = sc.next();
 
+        System.out.println();
         System.out.println("======== 예약하시려는 분의 정보를 확인해주세요. ========");
+        System.out.println();
         System.out.println("이름" + ":" + " " + name);
         System.out.println("전화번호" + ":" + " " + phoneNumber);
         System.out.println("소지금" + ":" + " " + money + "원");
-        System.out.println("예약일" + ":" + date);
+        System.out.println("예약일" + ":" + " " + date);
+        System.out.println();
         System.out.println("======== 확인하신 내용이 맞다면 1번, 다시 시도하시려면 2번을 입력해주세요. ========");
+        System.out.println();
 
         int confirm = sc.nextInt();
 
         if (confirm==1) {
+            System.out.println();
             System.out.println("======== 예약이 완료되었습니다. 이용해주셔서 감사합니다. ========");
             System.out.println();
+            System.out.println("==== 예약정보 ====");
             System.out.println("이름" + ":" + " " + name);
             System.out.println("전화번호" + ":" + " " + phoneNumber);
             System.out.println("소지금" + ":" + " " + money + "원");
-            System.out.println("예약일" + ":" + date );
+            System.out.println("예약일" + ":" + " " + date );
+            System.out.println();
             Guest guest = new Guest( name, phoneNumber, date, money);
             return guest;
 
         } else if (confirm==2) {
+            System.out.println();
             System.out.println("======== 올바른 정보를 다시 입력해주세요. ========");
             return inputGuest();
 
         } else {
+            System.out.println();
             System.out.println("======== 잘못 입력하셨습니다. 메인화면으로 돌아갑니다. ========");
+            System.out.println();
         }
         return null;
     }
 
-
+    // date 예약날짜를 개인정보 확인 후 입력하려다가 실패했습니다 ㅠ
     /*public Guest inputDate() {
         Scanner sc = new Scanner(System.in);
         System.out.println("======== 원하시는 예약 날짜를 입력해주세요. ========");
