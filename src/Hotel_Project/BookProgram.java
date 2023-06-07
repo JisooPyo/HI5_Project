e435907bc986650303d593c82eb455c006492126
JisooPyo <audwl326@gmail.com>
2023-06-07 09:20:16 +0900

    BookProgram 클래스 guestScreen() 메서드 수정
    guest 정보가 맞는지 확인할 때, 1번도 2번도 누르지 않으면 메인 화면으로 돌아가야 하기 때문에
    guestScreen()에서 g가 null일경우 onProgram() 메서드로 다시 되돌아가는 코드를 추가하였다.

--- src/Hotel_Project/BookProgram.java
+++ src/Hotel_Project/BookProgram.java
@@ -7,34 +7,34 @@ public class BookProgram {
     Booking booking = new Booking();
     Scanner sc = new Scanner( System.in );
 
-    public void onProgram(){
-        System.out.println("[ HI5 호텔 예약 프로그램 ]\n");
-        System.out.println("안녕하세요. HI5 호텔입니다.");
-        System.out.println("원하시는 옵션을 선택해 주세요.\n");
-        System.out.println("1. 예약");
-        System.out.println("2. 예약조회");
-        System.out.println("3. 전체 예약 목록 조회(비밀번호 입력)");
-        System.out.println("4. 프로그램 종료");
+    public void onProgram() {
+        System.out.println( "[ HI5 호텔 예약 프로그램 ]\n" );
+        System.out.println( "안녕하세요. HI5 호텔입니다." );
+        System.out.println( "원하시는 옵션을 선택해 주세요.\n" );
+        System.out.println( "1. 예약" );
+        System.out.println( "2. 예약조회" );
+        System.out.println( "3. 전체 예약 목록 조회(비밀번호 입력)" );
+        System.out.println( "4. 프로그램 종료" );
         int pick = sc.nextInt();
-        switch ( pick ){
-            case 1:{
+        switch ( pick ) {
+            case 1: {
                 guestScreen();
                 break;
             }
-            case 2:{
+            case 2: {
                 checkBookingGuest();
                 break;
             }
-            case 3:{
+            case 3: {
                 checkBookingManager();
                 break;
             }
-            case 4:{
+            case 4: {
                 offProgram();
                 break;
             }
-            default:{
-                System.out.println("잘못된 입력입니다. 다시 입력해 주세요.");
+            default: {
+                System.out.println( "잘못된 입력입니다. 다시 입력해 주세요." );
                 onProgram();
                 break;
             }
@@ -42,29 +42,32 @@ public class BookProgram {
 
     }
 
-    public void guestScreen(){
+    public void guestScreen() {
         Guest g = guest.inputGuest();
+        if ( g == null ) {
+            onProgram();
+        }
         bookingScreen( g );
     }
 
-    public void bookingScreen(Guest g){
+    public void bookingScreen( Guest g ) {
         Booking b = booking.makeBooking( g );
         booking.bookingList.add( b );
         booking.idNumber++;
         onProgram();
     }
 
-    public void checkBookingManager(){
+    public void checkBookingManager() {
         booking.showBookingToManager();
         onProgram();
     }
 
-    public void checkBookingGuest(){
+    public void checkBookingGuest() {
         booking.showBookingToGuest();
         onProgram();
     }
 
-    public void offProgram(){
-        System.out.println("프로그램을 종료합니다.");
+    public void offProgram() {
+        System.out.println( "프로그램을 종료합니다." );
     }
 }
