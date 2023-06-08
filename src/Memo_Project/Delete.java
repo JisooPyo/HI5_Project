4ddb0c33e1527ea9255f9f8b0ec36722bce51187
JisooPyo <audwl326@gmail.com>
2023-06-08 21:32:25 +0900

    Delete 클래스 checkPassword 메서드 조건문 추가
    메서드에서 int값이 아니라 다른 값을 입력하였을 때 다시 선택할 수 있게 하는 조건문을 추가하였다.

--- src/Memo_Project/Delete.java
+++ src/Memo_Project/Delete.java
@@ -7,7 +7,7 @@ public class Delete {
     Scanner sc = new Scanner( System.in );
 
     // number를 받아서 map의 keyset에 number가 있으면 checkPassword() 호출 없으면 notContain() 메서드 호출
-    public void delete() {
+    public void delete () {
 
         System.out.println( "삭제할 글의 번호를 입력해 주세요." );
         int number = sc.nextInt();
@@ -22,12 +22,13 @@ public class Delete {
     }
 
     // num에 관한 글이 존재하지 않을 때 메인 화면으로 돌아가는 메서드
-    public void notContain() {
+    public void notContain () {
 
         System.out.println( "삭제할 글이 존재하지 않습니다." );
         System.out.println( "메인 화면으로 돌아가시려면 1번, 다시 번호를 입력하시려면 2번을 입력해 주세요." );
 
         int n = sc.nextInt();
+        String enter = sc.nextLine();
         if ( n == 1 ) {
         } else if ( n == 2 ) {
             delete();
@@ -39,7 +40,7 @@ public class Delete {
     }
 
     // 비밀번호를 체크해서 맞으면 메모를 삭제해주는 메서드
-    public void checkPassword( int number ) {
+    public void checkPassword ( int number ) {
         System.out.println( "비밀번호를 입력해 주세요." );
 
         String pw = sc.nextLine();
@@ -47,8 +48,15 @@ public class Delete {
             System.out.println( "비밀번호가 일치하지 않습니다." );
             System.out.println( "메인 화면으로 돌아가시려면 1번, 다시 비밀번호를 입력하시려면 2번을 입력해 주세요." );
 
+            if ( !sc.hasNextInt() ) {
+                System.out.println( "잘못된 값을 입력하셨습니다. 다시 선택해 주세요.\n" );
+                String wrongValue = sc.next();
+                checkPassword( number );
+            }
+
             int n = sc.nextInt();
             if ( n == 1 ) {
+                System.out.println( "메인 화면으로 돌아갑니다.\n" );
             } else if ( n == 2 ) {
                 checkPassword( number );
             } else {
@@ -61,7 +69,7 @@ public class Delete {
 
             Memo.mapPassword.remove( number );
             Memo.mapMemo.remove( number );
-            System.out.println("메모가 삭제되었습니다. 첫 화면으로 돌아갑니다.");
+            System.out.println( "메모가 삭제되었습니다. 첫 화면으로 돌아갑니다." );
 
         }
 
