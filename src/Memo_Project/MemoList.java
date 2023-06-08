06b97fc93590ba0ac2ba2d604c89c61bf6819340
JisooPyo <audwl326@gmail.com>
2023-06-08 21:31:29 +0900

    MemoList 클래스 출력부 수정
    MemoList 클래스를 출력할 때 더 자연스럽게 나오도록 출력부 글을 수정하였다.

--- src/Memo_Project/MemoList.java
+++ src/Memo_Project/MemoList.java
@@ -1,39 +1,38 @@
 package Memo_Project;
 
 import java.util.Map;
-import java.util.Scanner;
 
 public class MemoList {
 
-    public void save( Memo memo ) {
+    public void save ( Memo memo ) {
         Memo.mapMemo.put( memo.number, memo );
         Memo.mapPassword.put( memo.number, memo.password );
     }
 
-    public void getMemoList() {
+    public void getMemoList () {
         if ( Memo.mapMemo.size() == 0 ) {
             System.out.println( "출력할 메모가 없습니다." );
         } else {
-            int maxNum = maxNum(Memo.mapMemo);
-            for ( int i = 1 ; i <= maxNum ; i++ ) {
+            int maxNum = maxNum( Memo.mapMemo );
+            for ( int i = 1; i <= maxNum; i++ ) {
                 if ( Memo.mapMemo.keySet().contains( i ) ) {
                     int number = Memo.mapMemo.get( i ).number;
                     String name = Memo.mapMemo.get( i ).name;
                     String date = Memo.mapMemo.get( i ).date;
                     String note = Memo.mapMemo.get( i ).note;
-                    System.out.println( number + " : " + name );
-                    System.out.println( "date : " + date );
-                    System.out.println( "note :" );
-                    System.out.println( note );
+                    System.out.println( number + "번, " + name + "님의 메모입니다." );
+                    System.out.println( "작성일시 : " + date );
+                    System.out.println( "메모내용 : " );
+                    System.out.println( note + "\n" );
                 }
             }
         }
     }
 
-    public int maxNum ( Map<Integer,Memo> mapMemo ) {
-        int maxNum=0;
-        for(Integer integer : mapMemo.keySet()) {
-            maxNum=Math.max(integer.intValue(), maxNum);
+    public int maxNum ( Map< Integer, Memo > mapMemo ) {
+        int maxNum = 0;
+        for ( Integer integer : mapMemo.keySet() ) {
+            maxNum = Math.max( integer.intValue(), maxNum );
         }
         return maxNum;
     }
