c3cc89253313350b5e4ee081de212a8ace02543e
JisooPyo <audwl326@gmail.com>
2023-06-09 11:18:37 +0900

    Booking 클래스 수정
    1. showBookingToManager() 메서드 구현
    2. Booking 클래스 접근제어자 정리
    3. showBookingToGuest() 메서드 설명 주석 추가

--- src/Hotel_Project/Booking.java
+++ src/Hotel_Project/Booking.java
@@ -3,12 +3,12 @@ package Hotel_Project;
 import java.util.*;
 
 public class Booking {
-    Guest guest;
-    Room room;
+    private Guest guest;
+    private Room room;
     Integer idNumber = 1000;
-    Hotel hotel = new Hotel();
+    private Hotel hotel = new Hotel();
     Scanner sc = new Scanner( System.in );
-    Map< Integer, Queue< Room > > mapRoom = hotel.makeMapRoom();
+    private Map< Integer, Queue< Room > > mapRoom = hotel.makeMapRoom();
     // Integer에는 1,2,3,4가 들어갑니다.
     Map< Integer, Booking > mapBooking = new HashMap<>();
     // Integer에는 idNumber가 들어갑니다.
@@ -49,7 +49,7 @@ public class Booking {
 
     }
 
-    public int listRoom ( Guest guest ) {
+    private int listRoom ( Guest guest ) {
         // 방 고르기
         int emptyRoom; // 잔여 룸 수
         int roomPrice; // 룸 가격
@@ -127,7 +127,7 @@ public class Booking {
 //        }
     }
 
-    public int canBook ( int chooseRoom, int emptyRoom, int roomPrice, String roomSize, Guest guest ) {
+    private int canBook ( int chooseRoom, int emptyRoom, int roomPrice, String roomSize, Guest guest ) {
         // 방 예약
 
         if(emptyRoom > 0 && roomPrice <= guest.money){
@@ -153,7 +153,7 @@ public class Booking {
         }
     }
 
-    public int cantBook ( int emptyRoom, int roomPrice, Guest guest ) {
+    private int cantBook ( int emptyRoom, int roomPrice, Guest guest ) {
         // 게스트의 소지금, 혹은 호텔 방 부족 문제로 예약 불가 상황
         if ( emptyRoom == 0 ) {  // 선택 한 룸에 방이 없을 경우
             System.out.println( "해당 객실은 현재 이용할 수 없습니다." );
@@ -168,39 +168,60 @@ public class Booking {
         return listRoom( guest );
     }
 
-    ///////////////////////// 채원님 구현부 ////////////////////////////////////////////////
-
-
+    ///////////////////////// 지수구현부.. ////////////////////////////////////////////////
 
 
     public void showBookingToManager () {
-        //bookingList 이용해서 for문 만들어서 출력하기. - 채원님
+        // 예약목록(bookingList)이 비어있으면 메인화면으로 돌아간다.
+        if ( bookingList.size() == 0 ) {
+            System.out.println( "예약목록이 없습니다." );
+            System.out.println( "메인화면으로 돌아갑니다.\n" );
+            return;
+        }
+
+        // 비밀번호를 입력해야 전체 예약 목록을 확인할 수 있다.
+        System.out.println( "예약현황을 보시려면 비밀번호가 필요합니다." );
+        System.out.println( "비밀번호를 입력해 주세요." );
+
+        // 비밀번호가 일치하지 않을 시, 메인 화면으로 되돌아간다.
+        String password = sc.next();
+        if ( !password.equals( "HI5HOTEL" ) ) {
+            System.out.println( "비밀번호가 일치하지 않습니다." );
+            System.out.println( "메인화면으로 이동합니다.\n" );
+            return;
+        }
+
+        // 비밀번호가 일치할 시 for문으로 전체 예약 목록을 조회할 수 있다.
+        System.out.println( "[ HI5 호텔 전체 예약 목록 조회 ]\n" );
+        for ( int i = 0; i < bookingList.size(); i++ ) {
+            System.out.println( "--------   idNumber : " + bookingList.get( i ).idNumber + "   --------" );
+            System.out.println( "날짜 : " + bookingList.get( i ).guest.date );
+            System.out.println( "이름 : " + bookingList.get( i ).guest.name );
+            System.out.println( "연락처 : " + bookingList.get( i ).guest.phoneNumber );
+            System.out.println( "객실 사이즈 : " + bookingList.get( i ).room.size + "\n" );
+        }
+
+        // 전체 예약 목록을 모두 조회하고 나면 메인화면으로 되돌아간다.
+        System.out.println("메인화면으로 돌아갑니다.");
     }
-
-
-
-
 
 
     ////////////////////////// 지수 구현부/////////////////////////////////////////////////
 
     public void showBookingToGuest () {
-        // mapBooking을 이용하여 idNumber에 맞는 Value값(Booking)을 찾아옵니다.
-        // idNumber가 일치하면 [예약 현황]을 보여주고 1. 메인메뉴 2. 예약취소를 출력합니다.
-        // 2번을 선택하면 예약을 취소할지 물어보고 1. 취소 2. 취소 안함
-        // 1번을 선택하면 예약 취소 -> mapRoom 추가, mapBooking 빼기, bookingList 빼기
-
-        // bookingList가 비어있을 경우 예약현황이 없음을 알려주고 메인화면으로 돌아갑니다.
+        // 예약목록(bookingList)이 비어있으면 메인화면으로 돌아간다.
         if ( bookingList.size() == 0 ) {
             System.out.println( "예약현황이 없습니다." );
             System.out.println( "메인화면으로 돌아갑니다.\n" );
             return;
         }
+
+        // 예약목록 조회를 위해 idNumber을 받는다.
         System.out.println( "[ 예약조회 ] ( 고객용 )\n" );
         System.out.println( "예약조회를 위해 idNumber가 필요합니다." );
         System.out.println( "idNumber을 입력해주세요." );
 
-        // 입력받은 idNumber에 따른 선택지입니다.
+        // 입력받은 idNumber에 따른 선택지.
         // idNumber에 맞는 예약 현황이 있는 경우 예약 현황을 보여주는 bookingCheck() 메서드로 넘어가고,
         // 맞지 않을 경우 메인 화면으로 돌아갑니다.
         Integer inputIdNum = Integer.valueOf( sc.next() );
@@ -210,33 +231,33 @@ public class Booking {
             return;
         }
         bookingCheck( inputIdNum );
-
     }
 
-    public void bookingCheck ( Integer inputIdnum ) {
+    private void bookingCheck ( Integer inputIdnum ) {
         // mapBooking.get( inputIdnum ) 을 통해 Booking 객체를 얻어와 그에 대한 정보를 출력.
         System.out.println( "[ 예약현황 ]\n" );
         System.out.println( "예약번호 : " + inputIdnum );
         System.out.println( "이름 : " + mapBooking.get( inputIdnum ).guest.name );
         System.out.println( "연락처 : " + mapBooking.get( inputIdnum ).guest.phoneNumber );
         System.out.println( "예약날짜 : " + mapBooking.get( inputIdnum ).guest.date );
-        System.out.println( "객실크기 : " + mapBooking.get( inputIdnum ).room.size +"\n");
+        System.out.println( "객실크기 : " + mapBooking.get( inputIdnum ).room.size + "\n" );
         System.out.println( "1. 메인메뉴 / 2. 예약 취소" );
 
-        // 1을 누르면 메인메뉴로 돌아가고
-        // 2를 누르면 예약 취소가 가능한 cancelBookingCheck() 메서드로 넘어갑니다.
         int cancelBooking = sc.nextInt();
         String enter = sc.nextLine();
 
         switch ( cancelBooking ) {
             case 1: {
+                // 1을 누르면 메인메뉴로 돌아간다.
                 return;
             }
             case 2: {
+                // 2를 누르면 예약 취소가 가능한 cancelBookingCheck() 메서드로 넘어간다.
                 cancelBookingCheck( inputIdnum );
                 break;
             }
             default: {
+                // 1, 2의 값이 아닌 다른 값을 입력하면 현재 메서드로 되돌아온다.
                 System.out.println( "잘못된 값을 입력하셨습니다. 다시 입력해 주세요." );
                 bookingCheck( inputIdnum );
                 break;
@@ -244,34 +265,42 @@ public class Booking {
         }
     }
 
-    public void cancelBookingCheck ( Integer inputIdNum ) {
+    private void cancelBookingCheck ( Integer inputIdNum ) {
         System.out.println( "예약을 취소하시겠습니까?" );
         System.out.println( "1. 예 / 2. 아니오" );
         int cancelPick = sc.nextInt();
         String enter = sc.nextLine();
         switch ( cancelPick ) {
             case 1: {
-                // 예약이 취소되었으므로
-                // 예약한 방을 다시 mapRoom에 추가해줍니다.
-                // bookingList에서 Booking 객체를 제거합니다.
-                // mapBooking에서 Key값이 idNumber인 Booking을 제거합니다.
+                // 예약 취소
+
                 String roomSize = mapBooking.get( inputIdNum ).room.size;
                 int roomPrice = mapBooking.get( inputIdNum ).room.price;
+
+                // 예약한 방을 다시 mapRoom에 추가.
+                // getRoomNumber메서드는 roomSize에 따라 mapRoom의 key값을 알려주는 메서드이다.
+                // ex. "single" -> (Key)1 -> (Value)SingleQueue
                 mapRoom.get( getRoomNumber( roomSize ) ).add( new Room( roomSize, roomPrice ) );
-                //////////////////////////////////////////////
-                // bookingList 잘 지워지는지 체크해봐야 함. //
-                //////////////////////////////////////////////
+
+                // bookingList에서 Booking 객체를 제거.
                 bookingList.remove( mapBooking.get( inputIdNum ) );
+
+                // mapBooking에서 Key값이 idNumber인 Booking을 제거.
                 mapBooking.remove( inputIdNum );
+
                 System.out.println( "예약이 성공적으로 취소되었습니다.\n" );
+
+                // 메인 화면으로 되돌아간다.
                 return;
             }
             case 2: {
+                // 자신의 예약현황을 볼 수 있는 bookingCheck() 메서드로 되돌아간다.
                 System.out.println( "예약현황으로 돌아갑니다.\n" );
                 bookingCheck( inputIdNum );
                 break;
             }
             default: {
+                // 1, 2의 값이 아닌 다른 값을 입력하면 현재 메서드로 되돌아온다.
                 System.out.println( "잘못된 값을 입력하셨습니다. 다시 입력해 주세요." );
                 cancelBookingCheck( inputIdNum );
                 break;
@@ -279,7 +308,7 @@ public class Booking {
         }
     }
 
-    public Integer getRoomNumber ( String size ) {
+    private Integer getRoomNumber ( String size ) {
         // size를 입력받아 size에 맞는 mapRoom의 key값을 알려주는 메서드.
         if ( size.equals( "single" ) ) {
             return 1;
